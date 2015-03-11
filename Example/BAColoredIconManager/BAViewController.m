232018d59d2c83b44a9530b6321ab4adf083712a
Marcus Fleischmann <marcus@bitartists.com>
2015-03-11 08:44:13 +0100

    Added table view with random colors to example app.

--- Example/BAColoredIconManager/BAViewController.m
+++ Example/BAColoredIconManager/BAViewController.m
@@ -10,9 +10,15 @@
 
 #import <BAColoredIconManager/BAColoredIconManagerFactory.h>
 
-@interface BAViewController ()
+typedef NS_ENUM(NSUInteger, SectionType) { SectionTypeRandom, SectionTypeRed, SectionTypeGreen, SectionTypeBlue };
 
-@property(weak, nonatomic) IBOutlet UIImageView *imageView;
+@interface BAViewController () <UITableViewDataSource>
+
+@property(weak, nonatomic) IBOutlet UITableView *tableView;
+@property(nonatomic) id<BAColoredIconManager> randomManager;
+@property(nonatomic) id<BAColoredIconManager> redManager;
+@property(nonatomic) id<BAColoredIconManager> greenManager;
+@property(nonatomic) id<BAColoredIconManager> blueManager;
 
 @end
 
@@ -22,10 +28,72 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    id<BAColoredIconManager> manager = [BAColoredIconManagerFactory manager];
-    manager.color = [UIColor greenColor];
-    UIImage *redIcon = [manager iconNamed:@"testicon"];
-    self.imageView.image = redIcon;
+    self.randomManager = [BAColoredIconManagerFactory manager];
+
+    self.redManager = [BAColoredIconManagerFactory manager];
+    self.redManager.color = [UIColor redColor];
+
+    self.greenManager = [BAColoredIconManagerFactory manager];
+    self.greenManager.color = [UIColor greenColor];
+
+    self.blueManager = [BAColoredIconManagerFactory manager];
+    self.blueManager.color = [UIColor blueColor];
+
+    self.tableView.scrollsToTop = YES;
 }
 
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 4;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 50;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+    if (indexPath.section == SectionTypeRandom) {
+        UIColor *randomColor = [self randomColor];
+        self.randomManager.color = randomColor;
+        cell.imageView.image = [self.randomManager iconNamed:@"testicon"];
+    } else if (indexPath.section == SectionTypeRed) {
+        cell.imageView.image = [self.redManager iconNamed:@"testicon"];
+    } else if (indexPath.section == SectionTypeGreen) {
+        cell.imageView.image = [self.greenManager iconNamed:@"testicon"];
+    } else if (indexPath.section == SectionTypeBlue) {
+        cell.imageView.image = [self.blueManager iconNamed:@"testicon"];
+    }
+
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %li - %li", @"Cell", indexPath.section, indexPath.row];
+
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSString *title = @"";
+
+    if (section == SectionTypeRandom) {
+        title = @"Random";
+    } else if (section == SectionTypeRed) {
+        title = @"Red";
+    } else if (section == SectionTypeGreen) {
+        title = @"Green";
+    } else if (section == SectionTypeBlue) {
+        title = @"Blue";
+    }
+
+    return title;
+}
+
+#pragma mark - Minions
+
+- (UIColor *)randomColor {
+    CGFloat hue = (arc4random() % 256 / 256.0);              //  0.0 to 1.0
+    CGFloat saturation = (arc4random() % 128 / 256.0) + 0.5; //  0.5 to 1.0, away from white
+    CGFloat brightness = (arc4random() % 128 / 256.0) + 0.5; //  0.5 to 1.0, away from black
+    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+}
 @end
